94e3d1ab66d2dc7d94821fdd52852c2a9e0c025c
Konstantinos Andrikopoulos <mandragore@protonmail.com>
2021-10-22 04:36:39 +0200

    pass third test

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -8,6 +8,8 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
     let mut builder_inners = quote!();
     let mut builder_constructor_inners = quote!();
+    let mut builder_methods = quote!();
+
     let ident = ast.ident;
     let builder_ident = format_ident!("{}Builder", ident);
     let data_struct = match ast.data {
@@ -21,6 +23,12 @@ pub fn derive(input: TokenStream) -> TokenStream {
                 let ident = field.ident.as_ref().unwrap();
                 builder_inners.extend(quote! { #ident: Option<#ty>, });
                 builder_constructor_inners.extend(quote! { #ident: None, });
+                builder_methods.extend(quote! {
+                    pub fn #ident(&mut self, #ident: #ty) -> &mut Self {
+                        self.#ident = Some(#ident);
+                        self
+                    }
+                });
             }
         }
         _=> unimplemented!(),
@@ -29,6 +37,10 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let expanded = quote! {
         pub struct #builder_ident {
             #builder_inners
+        }
+
+        impl #builder_ident {
+            #builder_methods
         }
 
         impl #ident {
