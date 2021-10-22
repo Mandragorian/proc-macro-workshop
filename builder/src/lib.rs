94a22175099fc3363dd7e1d93ed6b41ea00f4237
Konstantinos Andrikopoulos <mandragore@protonmail.com>
2021-10-23 01:11:19 +0200

    pass fifth and sixth tests

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -22,23 +22,81 @@ pub fn derive(input: TokenStream) -> TokenStream {
         syn::Fields::Named(fields) => {
             for field in fields.named.iter() {
                 let ty = &field.ty;
+                let (is_optional, inner) = if let syn::Type::Path(tpath) = ty {
+                    let mut iter = tpath.path.segments.iter();
+                    let must_be_option = iter.next();
+                    let must_be_none = iter.next();
+
+                    let (start_with_option, argument) = if let Some(segment) = must_be_option {
+                        let is_option = segment.ident == "Option";
+
+                        let argument_opt = if is_option {
+                            match &segment.arguments {
+                                syn::PathArguments::AngleBracketed(args) => {
+                                    let mut args_iter = args.args.iter();
+                                    args_iter.next().map(|generic_arg| {
+                                        match generic_arg {
+                                            syn::GenericArgument::Type(ty) => Some(ty),
+                                            _ => None,
+                                        }
+                                    }).flatten()
+                                }
+                                _ => panic!("non angle bracketed args in type"),
+                            }
+                        } else {
+                            None
+                        };
+                        (is_option, argument_opt)
+                    } else {
+                        (false, None)
+                    };
+
+                    (start_with_option && must_be_none.is_none(), argument)
+                } else {
+                    panic!("type wasn't path");
+                };
+
                 let ident = field.ident.as_ref().unwrap();
-                builder_inners.extend(quote! { #ident: Option<#ty>, });
+                if is_optional {
+                    builder_inners.extend(quote! { #ident: #ty, });
+                } else {
+                    builder_inners.extend(quote! { #ident: Option<#ty>, });
+                }
                 builder_constructor_inners.extend(quote! { #ident: None, });
-                builder_methods.extend(quote! {
-                    pub fn #ident(&mut self, #ident: #ty) -> &mut Self {
-                        self.#ident = Some(#ident);
-                        self
-                    }
-                });
-                builder_checks.extend(quote! {
-                    if self.#ident.is_none() {
-                        return Err("no fields can be None");
-                    }
-                });
-                target_construction.extend(quote! {
-                    #ident: self.#ident.unwrap(),
-                });
+
+                if is_optional {
+                    builder_methods.extend(quote! {
+                        pub fn #ident(&mut self, #ident: #inner) -> &mut Self {
+                            self.#ident = Some(#ident);
+                            self
+                        }
+                    });
+                } else {
+                    builder_methods.extend(quote! {
+                        pub fn #ident(&mut self, #ident: #ty) -> &mut Self {
+                            self.#ident = Some(#ident);
+                            self
+                        }
+                    });
+                }
+
+                if !is_optional {
+                    builder_checks.extend(quote! {
+                        if self.#ident.is_none() {
+                            return Err("no fields can be None");
+                        }
+                    });
+                }
+
+                if !is_optional {
+                    target_construction.extend(quote! {
+                        #ident: self.#ident.clone().unwrap(),
+                    });
+                } else {
+                    target_construction.extend(quote! {
+                        #ident: self.#ident.clone(),
+                    });
+                }
             }
         }
         _=> unimplemented!(),
@@ -52,7 +110,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
         impl #builder_ident {
             #builder_methods
 
-            pub fn build(self) -> Result<#ident, &'static str> {
+            pub fn build(&mut self) -> Result<#ident, &'static str> {
                 #builder_checks
 
                 Ok(Command {
