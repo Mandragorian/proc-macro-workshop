a111b68622104ce3293278e911bf26c796e315f4
Konstantinos Andrikopoulos <mandragore@protonmail.com>
2021-10-26 00:44:23 +0200

    pass eigth test

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,7 +1,8 @@
 use proc_macro::TokenStream;
-use proc_macro2::TokenStream as TokenStream2;
-use quote::{format_ident, quote};
+use proc_macro2::{TokenStream as TokenStream2, Span};
+use quote::{format_ident, quote, quote_spanned};
 use syn;
+use syn::spanned::Spanned;
 
 fn get_data_struct_from_ast_data(ast_data: syn::Data) -> syn::DataStruct {
     match ast_data {
@@ -26,15 +27,21 @@ fn get_optional_argument_from_type_arguments(segment: &syn::PathSegment) -> Opti
     }
 }
 
+enum BuilderError {
+    Error(String, Span),
+}
+
 fn get_field_builder_type<'a>(
     optional_segment: Option<&'a syn::PathSegment>,
     attrs: &'a Vec<syn::Attribute>,
     ident: &'a syn::Ident,
-) -> FieldBuilderType<'a> {
-    let our_attrs = attrs.iter()
-        .filter_map(|attr| attr.parse_meta().ok() )
-        .find_map(|meta: syn::Meta| {
-            match meta {
+) -> Result<FieldBuilderType<'a>, BuilderError> {
+    let filtered_attrs = attrs.iter()
+        .filter_map(|attr| attr.parse_meta().ok() );
+        //.find_map(|meta: syn::Meta| {
+    let mut our_attrs: Option<String> = None;
+    for meta in filtered_attrs {
+            match &meta {
                 syn::Meta::List(meta_list) => {
                     if meta_list.path.is_ident("builder") {
                         let f = meta_list.nested.first().unwrap();
@@ -42,51 +49,46 @@ fn get_field_builder_type<'a>(
                             if let syn::Meta::NameValue(name_value) = v {
                                 if name_value.path.is_ident("each") {
                                     if let syn::Lit::Str(lit) = &name_value.lit {
-                                        Some(lit.value())
+                                        our_attrs = Some(lit.value());
                                     } else {
                                         panic!("expected string literal");
                                     }
                                 } else {
-                                    panic!("unrecognized attribute");
+                                    let error_msg = format!("expected `builder(each = \"...\")`");
+                                    return Err(BuilderError::Error(error_msg, meta.span()));
                                 }
-                            } else {
-                                None
                             }
-                        } else {
-                            None
                         }
-                    } else {
-                        None
                     }
                 }
-                _ => None,
+                _ => (),
             }
-        });
+        };
 
     if let Some(segment) = optional_segment {
         match segment.ident.to_string().as_str() {
             "Option" => {
                 let argument_opt = get_optional_argument_from_type_arguments(segment);
-                FieldBuilderType::Optional(argument_opt.unwrap())
+                Ok(FieldBuilderType::Optional(argument_opt.unwrap()))
             },
             "Vec" => {
                 let argument_opt = get_optional_argument_from_type_arguments(segment);
                 if let Some(each_func) = our_attrs {
                     if ident != each_func.as_str() {
-                        FieldBuilderType::Vectoral(argument_opt.unwrap(), each_func)
+                        Ok(FieldBuilderType::Vectoral(argument_opt.unwrap(), each_func))
                     } else {
-                        FieldBuilderType::VectoralNoSetAll(argument_opt.unwrap(), each_func)
+                        Ok(FieldBuilderType::VectoralNoSetAll(argument_opt.unwrap(), each_func))
                     }
                 } else {
-                    FieldBuilderType::Simple
+                    Ok(FieldBuilderType::Simple)
                 }
             }
             _ => {
-                FieldBuilderType::Simple
+                Ok(FieldBuilderType::Simple)
             }
         }
     } else {
-        FieldBuilderType::Simple
+        Ok(FieldBuilderType::Simple)
     }
 }
 
@@ -97,7 +99,7 @@ enum FieldBuilderType<'a> {
     VectoralNoSetAll(&'a syn::Type, String),
 }
 
-fn determine_field_type<'a>(ty: &'a syn::Type, attrs: &'a Vec<syn::Attribute>, field_ident: &'a syn::Ident) -> FieldBuilderType<'a> {
+fn determine_field_type<'a>(ty: &'a syn::Type, attrs: &'a Vec<syn::Attribute>, field_ident: &'a syn::Ident) -> Result<FieldBuilderType<'a>, BuilderError> {
     if let syn::Type::Path(tpath) = ty {
         let mut iter = tpath.path.segments.iter();
         let must_be_option = iter.next();
@@ -105,25 +107,25 @@ fn determine_field_type<'a>(ty: &'a syn::Type, attrs: &'a Vec<syn::Attribute>, f
 
         let res = get_field_builder_type(must_be_option, attrs, field_ident);
 
-        match res {
-            FieldBuilderType::Simple => FieldBuilderType::Simple,
+        match res? {
+            FieldBuilderType::Simple => Ok(FieldBuilderType::Simple),
             FieldBuilderType::Optional(inner) => {
                 if must_be_none.is_some() {
                     panic!("wrong type parameters for Option")
                 }
-                FieldBuilderType::Optional(inner)
+                Ok(FieldBuilderType::Optional(inner))
             }
             FieldBuilderType::Vectoral(inner, each_func) => {
                 if must_be_none.is_some() {
                     panic!("wrong type parameters for Vec")
                 }
-                FieldBuilderType::Vectoral(inner, each_func)
+                Ok(FieldBuilderType::Vectoral(inner, each_func))
             }
             FieldBuilderType::VectoralNoSetAll(inner, each_func) => {
                 if must_be_none.is_some() {
                     panic!("wrong type parameters for Vec")
                 }
-                FieldBuilderType::VectoralNoSetAll(inner, each_func)
+                Ok(FieldBuilderType::VectoralNoSetAll(inner, each_func))
             }
         }
     } else {
@@ -138,17 +140,21 @@ fn handle_field(
     let ty = &field.ty;
     let attrs = &field.attrs;
     let ident = field.ident.as_ref().unwrap();
-    let field_builder_type = determine_field_type(ty, attrs, ident);
+    //let field_builder_type = determine_field_type(ty, attrs, ident);
 
-    match field_builder_type {
-        FieldBuilderType::Optional(inner) =>
-            builder.add_optional(ident, inner),
-        FieldBuilderType::Simple =>
-            builder.add_simple(ident, ty),
-        FieldBuilderType::Vectoral(inner, each_func) =>
-            builder.add_vectoral(ident, inner, each_func),
-        FieldBuilderType::VectoralNoSetAll(inner, each_func) =>
-            builder.add_vectoral_no_set_all(ident, inner, each_func),
+    match determine_field_type(ty, attrs, ident) {
+        Ok(field_builder_type) =>
+            match field_builder_type {
+            FieldBuilderType::Optional(inner) =>
+                builder.add_optional(ident, inner),
+            FieldBuilderType::Simple =>
+                builder.add_simple(ident, ty),
+            FieldBuilderType::Vectoral(inner, each_func) =>
+                builder.add_vectoral(ident, inner, each_func),
+            FieldBuilderType::VectoralNoSetAll(inner, each_func) =>
+                builder.add_vectoral_no_set_all(ident, inner, each_func),
+        }
+        Err(err) => builder.add_error(err),
     }
 }
 
@@ -158,7 +164,9 @@ struct BuilderBuilder {
     builder_methods: TokenStream2,
     builder_checks: TokenStream2,
     target_construction: TokenStream2,
+    errors: TokenStream2,
     ident: syn::Ident,
+    has_errors: bool,
 }
 
 impl BuilderBuilder {
@@ -168,6 +176,7 @@ impl BuilderBuilder {
         let builder_methods = quote!();
         let builder_checks = quote!();
         let target_construction = quote!();
+        let errors = quote!();
 
         BuilderBuilder {
             builder_inners,
@@ -176,6 +185,19 @@ impl BuilderBuilder {
             builder_checks,
             target_construction,
             ident,
+            errors,
+            has_errors: false,
+        }
+    }
+
+    fn add_error(&mut self, error: BuilderError) {
+        self.has_errors = true;
+        match error {
+            BuilderError::Error(msg, span) => {
+                self.errors.extend(quote_spanned! {
+                    span => compile_error!(#msg);
+                });
+            }
         }
     }
 
@@ -269,6 +291,7 @@ impl BuilderBuilder {
     }
 
     fn build(&self) -> TokenStream2 {
+        let errors = &self.errors;
         let builder_inners = &self.builder_inners;
         let builder_constructor_inners = &self.builder_constructor_inners;
         let builder_methods = &self.builder_methods;
@@ -277,7 +300,14 @@ impl BuilderBuilder {
         let ident = &self.ident;
         let builder_ident = format_ident!("{}Builder", ident);
 
+        if self.has_errors {
+            return quote! {
+                #errors
+            };
+        }
+
         quote! {
+            #errors
             pub struct #builder_ident {
                 #builder_inners
             }
